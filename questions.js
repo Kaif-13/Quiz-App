9f70a9c565c67b21f71aa87d881b73978b4e1005
Kaif-13 <125121440+Kaif-13@users.noreply.github.com>
2023-03-20 10:37:42 +0530

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,57 @@
+ let questions = [
+    {
+    numb: 1,
+    question: "HTML stand for_______",
+    answer: "Hyper Text Markup Language",
+    options: [
+      "Hyper Text Preprocessor",
+      "Hyper Text Markup Language",
+      "Hyper Text Multiple Language",
+      "Hyper Tool Multi Language"
+    ]
+  },
+    {
+    numb: 2,
+    question: "What is CSS?",
+    answer: "All of the mentioned",
+    options: [
+      "CSS is a style sheet language",
+      "CSS is a designed to separate this presentation and content, including layout, colors,and fonts",
+      "CSS is the language used to style the HTML documents",
+      "All of the mentioned"
+    ]
+  },
+    {
+    numb: 3,
+    question: "Which of the following CSS framework is used to create a responsive design?",
+    answer: "Bootstrap",
+    options: [
+      "Django",
+      "Rails",
+      "Larawell",
+      "Bootstrap"
+    ]
+  },
+    {
+    numb: 4,
+    question: "Which element  is used for or styling HTML5 layout?",
+    answer: "CSS",
+    options: [
+      "PHP",
+      "JQuery",
+      "Javascript",
+      "CSS"
+    ]
+  },
+    {
+    numb: 5,
+    question: "What is javascript?",
+    answer: "Javascript is an scripting language used to make the website interactive",
+    options: [
+       "Javascript is an scripting language used to make the website interactive",
+      "Javascript is a compiled language used to make the website interactive",
+      "Javascript is a assembly language used to make the website interactive",
+      "None of the mentioned"
+    ]
+  },
+];
